0fffd9ed98c86f233ffa99ac44abe92917b92918
uncopied <elian.carsenat@uncopied.art>
2022-02-14 16:19:57 +0100

    prepush from J's branch

--- src/screens/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import HomeScreen from './HomeScreen';
-export default function Home(){
-    return <div>
-                <HomeScreen>
-                    
-                </HomeScreen>
-            </div>
-}
